46cd1ba4728adba59bb184050c243c3ef039179b
Diego Munguia <diego.munguia@edify.cr>
2019-02-24 12:42:11 -0600

    Pruebas unitarias de servicios y mejora en el chequeo de salud de mysql

--- src/test/java/edu/tec/ic6821/sample/services/UserServiceTests.java
+++ src/test/java/edu/tec/ic6821/sample/services/UserServiceTests.java
@@ -1,14 +1,24 @@
 package edu.tec.ic6821.sample.services;
 
 import edu.tec.ic6821.sample.daos.UserDao;
+import edu.tec.ic6821.sample.model.User;
 import edu.tec.ic6821.sample.services.impl.UserServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 public class UserServiceTests {
@@ -18,12 +28,14 @@ public class UserServiceTests {
         @MockBean
         UserDao userDao;
 
-        @Autowired
-        PasswordEncoder passwordEncoder;
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 
         @Bean
         public UserService userService() {
-            return new UserServiceImpl(userDao, passwordEncoder);
+            return new UserServiceImpl(userDao, passwordEncoder());
         }
     }
 
@@ -32,4 +44,52 @@ public class UserServiceTests {
 
     @Autowired
     UserDao userDao;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Before
+    public void setUp() {
+        Mockito.when(userDao.existsByUsername("someexistinguser")).thenReturn(true);
+        Mockito.when(userDao.existsByUsername("somenewuser")).thenReturn(false);
+        Mockito.when(userDao.create(ArgumentMatchers.any(User.class)))
+                .thenAnswer(invocation -> {
+                   User createdUser = invocation.getArgument(0);
+                   createdUser.setId(42L);
+                   return createdUser;
+                });
+    }
+
+    @Test
+    public void whenCreateExistingUser_thenReturnEmpty() {
+        // given
+        String username = "someexistinguser";
+        String password = "123queso";
+
+        // when
+        Optional<User> result = userService.create(username, password);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void whenCreateNonExistingUser_thenReturnNewUser() {
+        // given
+        String username = "somenewuser";
+        String password = "123queso";
+
+        // when
+        Optional<User> result = userService.create(username, password);
+
+        // then
+        assertThat(result).isNotEmpty();
+
+        User newUser = result.get();
+        assertThat(newUser).hasFieldOrPropertyWithValue("id", 42L);
+        assertThat(passwordEncoder.matches(password, newUser.getPassword())).isTrue();
+    }
+
+
+
 }
